dfcbf2c21b9b9ad09912a02a090a6c87b6e38efe
Joseph <mcal5071@gmail.com>
2020-02-01 18:44:02 +1300

    Feat: Change from scanner input to command-line input.
    This uses the args String array from the main method to take in the encryption mode, shift key value, and String data.

--- src/com/hyperskill/Main.java
+++ src/com/hyperskill/Main.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        //Three arguments are used: -mode, -key, and -data.  Each are followed by their respective data.
 
-	    String operation = scanner.nextLine(); //"enc" for encrypt or "dec" for decrypt"
-	    String text = scanner.nextLine();
-	    int key = scanner.nextInt();
+        String operation = "enc";
+        int key = 0;
+        String data = "";
+
+        for(int i = 0; i < args.length; i++){
+            if(args[i].equals("-mode")){
+                operation = args[i + 1];
+            } else if (args[i].equals("-key")){
+                key = Integer.parseInt(args[i + 1]);
+            } else if (args[i].equals("-data")){
+                data = args[i + 1];
+            }
+        }
 
 	    switch (operation){
             case "enc":
-                encrypt(text, key);
+                encrypt(data, key);
                 break;
             case "dec":
-                decrypt(text, key);
+                decrypt(data, key);
                 break;
         }
 
